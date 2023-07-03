ee31411c718f6afb55743cdce129b0028b6d0919
Anton Nehaev <nehaev07.07.2003@gmail.com>
2023-07-03 15:40:01 +0300

    Add custom Tab bar

--- WordsFactory/WordsFactory/SceneDelegate.swift
+++ WordsFactory/WordsFactory/SceneDelegate.swift
@@ -18,8 +18,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let vc = ViewController()
-        window.rootViewController = vc
+//        let vc = ViewController()
+        let navigationConroller = UINavigationController(rootViewController: CustomTabBarController())
+        window.rootViewController = navigationConroller
         self.window = window
         window.makeKeyAndVisible()
     }
